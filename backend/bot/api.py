d1e82291b0ab4a3e2d2594bc75ba4926ee827385
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-02-25 07:05:05 +0700

    [bot] some fixes and admin

--- backend/bot/api.py
+++ backend/bot/api.py
@@ -1,8 +1,13 @@
+import logging
+from django.http import HttpResponse
 from ninja import NinjaAPI
+from pydantic import ValidationError
 
 from aiogram.types import Update
 
 from .bot import dp, bot
+
+logger = logging.getLogger(__name__)
 
 api = NinjaAPI(
     openapi_url=None,
@@ -12,5 +17,9 @@ api = NinjaAPI(
 
 @api.post("")
 async def index(request):
-    update = Update.model_validate_json(request.body)
-    return await dp.feed_update(bot, update)
+    try:
+        update = Update.model_validate_json(request.body)
+        return await dp.feed_update(bot, update)
+    except ValidationError as e:
+        logger.error("Invalid request", exc_info=True)
+        return HttpResponse(e.json(), status=400, content_type="application/json")
--- backend/bot/bot.py
+++ backend/bot/bot.py
@@ -1,14 +1,19 @@
+import logging
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 from asgiref.sync import sync_to_async
+
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters.command import CommandStart
+from aiogram.filters.command import CommandStart, Command
 from aiogram.types import Message
 
 from .models import TelegramUser
+
+logger = logging.getLogger(__name__)
 
 
 dp = Dispatcher()
@@ -25,11 +30,36 @@ async def start(message: Message):
     try:
         tg_user = await TelegramUser.objects.aget(id=message.from_user.id)
     except TelegramUser.DoesNotExist:
-        user = await get_user_model().objects.acreate(
-            username=message.from_user.username or f"tg{message.from_user.id}",
-            first_name=message.from_user.first_name,
-            last_name=message.from_user.last_name or "",
-        )
-        tg_user = await TelegramUser.objects.acreate(id=message.from_user.id, user=user)
+        try:
+            base_username = message.from_user.username or f"tg{message.from_user.id}"
+            username = base_username
+            suffix = 1
+            while True:
+                try:
+                    user = await get_user_model().objects.acreate(
+                        username=username,
+                        first_name=message.from_user.first_name,
+                        last_name=message.from_user.last_name or "",
+                    )
+                    break
+                except IntegrityError:
+                    username = f"{base_username}{suffix}"
+                    suffix += 1
+            tg_user = await TelegramUser.objects.acreate(
+                id=message.from_user.id, user=user
+            )
+        except Exception as e:
+            logger.error(e)
+            await message.answer(
+                "Sorry, an error occurred while creating your account. Please try again later."
+            )
+            raise
 
     await message.answer(f"Hello, {tg_user}!")
+
+
+@dp.message(Command("myplants"))
+async def myplants(message: Message):
+    plants = await TelegramUser.objects.aget(id=message.from_user.id)
+
+    await message.answer(str(plants))
